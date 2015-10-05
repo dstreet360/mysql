f4469f53b9320f2f21e21fd833cac45c6e342be6
dstreet360 <dstreet360@gmail.com>
2015-10-05 00:49:32 +0000

    first commit

---
+++ createdropdb.sql
@@ -0,0 +1,16 @@
+-- drop the table
+DROP TABLE inquiries;
+
+-- create the table
+CREATE TABLE inquiries
+(
+    id int NOT NULL AUTO_INCREMENT,
+    FirstName varchar(255) NOT NULL,   
+    LastName varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL,
+    Management int,
+    Marketing int,
+    GeneralBusiness int,
+    Economics int,
+    PRIMARY KEY(id)
+);
